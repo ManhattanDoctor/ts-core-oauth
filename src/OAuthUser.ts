eaf3952c9cf577eca0c58ac80fe5c9326b9c3e7c
Renat Gubaev <gubaev.r@n-t.io>
2024-06-19 14:09:33 +0300

    auto commit

--- src/OAuthUser.ts
+++ src/OAuthUser.ts
@@ -1,5 +1,5 @@
-import * as _ from 'lodash';
 import { Type } from 'class-transformer';
+import * as _ from 'lodash';
 
 export abstract class OAuthUser {
     //--------------------------------------------------------------------------
