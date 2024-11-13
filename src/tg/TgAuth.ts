c3cc8b1573213a67706505092f6ffbad4eb76c48
Renat Gubaev <gubaev.r@n-t.io>
2024-11-13 15:59:53 +0300

    auto commit

--- src/tg/TgAuth.ts
+++ src/tg/TgAuth.ts
@@ -5,6 +5,67 @@ import { ITgApiLoader } from "./TgApiLoader";
 import * as _ from 'lodash';
 
 export class TgAuth extends LoggerWrapper {
+
+    //--------------------------------------------------------------------------
+    //
+    // 	Static Methods
+    //
+    //--------------------------------------------------------------------------
+
+    public static getInitData(locationHash: string): any {
+        let item = TgAuth.urlParseHashParams(locationHash);
+        return item.tgWebAppData;
+    }
+
+    // Based on telegram-web-app.js
+    private static urlParseHashParams(locationHash: string): Record<string, any> {
+        locationHash = locationHash.replace(/^#/, '');
+        let params = {} as any;
+        if (_.isEmpty(locationHash)) {
+            return params;
+        }
+        if (locationHash.indexOf('=') < 0 && locationHash.indexOf('?') < 0) {
+            params._path = TgAuth.urlSafeDecode(locationHash);
+            return params;
+        }
+        let qIndex = locationHash.indexOf('?');
+        if (qIndex >= 0) {
+            var pathParam = locationHash.substr(0, qIndex);
+            params._path = TgAuth.urlSafeDecode(pathParam);
+            locationHash = locationHash.substr(qIndex + 1);
+        }
+        let query_params = TgAuth.urlParseQueryString(locationHash);
+        for (var k in query_params) {
+            params[k] = query_params[k];
+        }
+        return params;
+    }
+
+    private static urlParseQueryString(queryString: string): Record<string, any> {
+        var params = {};
+        if (!queryString.length) {
+            return params;
+        }
+        let queryStringParams = queryString.split('&');
+        let i, param, paramName, paramValue;
+        for (i = 0; i < queryStringParams.length; i++) {
+            param = queryStringParams[i].split(/=(.+)/)
+            paramName = TgAuth.urlSafeDecode(param[0]);
+            paramValue = param[1] == null ? null : TgAuth.urlSafeDecode(param[1]);
+            params[paramName] = paramValue;
+        }
+        return params;
+    }
+
+    private static urlSafeDecode(item: string): string {
+        try {
+            item = item.replace(/\+/g, '%20');
+            return decodeURIComponent(item);
+        } catch (error) {
+            return item;
+        }
+    }
+
     //--------------------------------------------------------------------------
     //
     // 	Properties
@@ -48,7 +109,7 @@ export class TgAuth extends LoggerWrapper {
                     user.parse(item);
                     this.promise.resolve(user);
                 })
-                
+
             })
             .catch(error => {
                 this.promise.reject(new ExtendedError(error.message));
